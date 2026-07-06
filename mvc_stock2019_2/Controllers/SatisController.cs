1db5c79c302969eba7cc73219fc0ab3449da39d1
Abdullah Dagli <abdullah.dagli@outlook.com>
2026-07-06 07:37:29 +0300

    satis modal popup

---
+++ mvc_stock2019_2/Controllers/SatisController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvc_stock2019_2.Controllers
+{
+    public class SatisController : Controller
+    {
+        // GET: Satis
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
